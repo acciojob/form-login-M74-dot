f3c2c5bc263a0e01d4ff8b6bf7be31d3b292382d
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-26 23:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
-
+	 event.preventDefault();
+	const form = document.getElementById('form1');
+    
+    // Retrieve the values of the input fields
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+    
+    // Display the full name using alert
+    alert(`${firstName} ${lastName}`);
 }
